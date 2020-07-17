63d29479b5b007593765dcf8eae27776d84ae7e2
Pranav Avva <pranav.avva@gmail.com>
2020-07-17 15:52:54 -0400

    move google analytics script directly to index.html

--- gtag.js
+++
@@ -1,8 +0,0 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-172949298-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-172949298-1');
-</script>
