65906440bbac07ddae465a0d60a1944c0cd9bdb9
Pranav Avva <pranav.avva@gmail.com>
2020-07-17 15:46:13 -0400

    google analytics

---
+++ gtag.js
@@ -0,0 +1,8 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-172949298-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-172949298-1');
+</script>
